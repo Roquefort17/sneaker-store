4eed904a4a61669920c7aa7c02f3a9d06c249bbb
Roquefort17 <165171374+Roquefort17@users.noreply.github.com>
2024-05-25 21:07:08 +0700

    Add files via upload

---
+++ project-shop-clothing/js/main.js
@@ -0,0 +1,490 @@
+let productData = [
+    {
+        id: 1,
+        imgUrl:
+            'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcSgmYbQuSzU3NJ08viCIPQvlcOMzxZpfX2FSE2izcCsHScQ_RVd4rN6Hfg0wM3zXDuj5IMwxImGng3HyrZ9QcBO_AsFQYu_g_-CU3zAUcgA5oZTD-uf0OQ_zO5j7odULz_t82THPS7M8Ak&usqp=CAc',
+        cardDesc: 'Кроссовки женские Nike Air Force - Yellow - Leather',
+        cardPrice: '8 907,50 ₽',
+        showWrapper: ` 
+        <img src="/img/view_product.svg" alt=""> 
+        <img src="/img/add_about_busket.svg" alt=""> 
+        `,
+        shoesSize: 38,
+        cardsGender: 'Баба'
+    },
+    {
+        id: 2,
+        imgUrl:
+            'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSZKhM42M2wlcEFRXsNNdz1jdSAuYZQ3abH4Ph0Es1qVpRA9SkLc0RB4JPMKWybKruJ5p17AErK1b22c4glzZ80oziy-hoD2-H3ut7pYdIcZEshn3LilkzbM8PUpy-pjwVvRgN5cg&usqp=CAc',
+        cardDesc: 'Кроссовки женские New Balance',
+        cardPrice: '9 456,36 ₽',
+        showWrapper: ` 
+        <img src="/img/view_product.svg" alt=""> 
+        <img src="/img/add_about_busket.svg" alt=""> 
+        `,
+        shoesSize: 37,
+        cardsGender: 'Баба'
+    },
+    {
+        id: 3,
+        imgUrl:
+            'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRXZFTulEjoCh2k5HvleJ8KFUm8Nl-1s6PI1pxIkT1L8PP5DFhnb9b6m0bVhmWQd2nhq4HOzsH9m0O08PCM46YEYnrAgHAVbc89eUP6qOTksIyJb5f7KJdHmrhqQYyBJKwwGpsICvGgZnQ&usqp=CAc',
+        cardDesc: 'Кроссовки женские Wide Laufschuh für Frauen',
+        cardPrice: '15 816,37 р',
+        showWrapper: ` 
+        <img src="/img/view_product.svg" alt=""> 
+        <img src="/img/add_about_busket.svg" alt=""> 
+        `,
+        shoesSize: 36,
+        cardsGender: 'Баба'
+    },
+    {
+        id: 4,
+        imgUrl:
+            'https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcS6nW-oqhGgu79rQxdw43rnf67VPZQKuad9ilJFHkxmA_PLRCuZ2VB_pPH7drna7FueFrmKCTMozvHVSvBoNlw0cqYDlhGkPnBLpZxvKcmMkIKKPTfRjl2F&usqp=CAc',
+        cardDesc: 'Кроссовки женские Neue Künstlerische Stil Nationaltide',
+        cardPrice: '1 381,82 ₽',
+        showWrapper: ` 
+        <img src="/img/view_product.svg" alt=""> 
+        <img src="/img/add_about_busket.svg" alt=""> 
+        `,
+        shoesSize: 39,
+        cardsGender: 'Баба'
+    },
+    {
+        id: 5,
+        imgUrl:
+            'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRoFFsEJMn6BVYtGXy7-29qk1SFdhMK176BjSYPAw0GmcuI-FBniUVxxcGHNxGq4EmuQB-MqEotZ0tTh5KKFrlL57XpHDSpPEFTp9KsSlofOtKQs2vLjb3iRCAO_8p0MD-J2NX62vI&usqp=CAc',
+        cardDesc: 'Кроссовки женские Sneaker in H-Weite',
+        cardPrice: '13 278,50 ₽',
+        showWrapper: ` 
+        <img src="/img/view_product.svg" alt=""> 
+        <img src="/img/add_about_busket.svg" alt=""> 
+        `,
+        shoesSize: 35,
+        cardsGender: 'Баба'
+    },
+    {
+        id: 6,
+        imgUrl:
+            'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSTq43Ub4LaVpFu60Ryb322CH05-IBUOcSD45Zsfnbx7p24KQmXGot45lmLFLUu_TQRD2KSA1P-Et-FDfABmQDWsybLB5Gr83L52rzw9M0SI6VZUwJwPrwUXw3hDPTR_BCor4F5jJw&usqp=CAc',
+        cardDesc: 'Кроссовки мужские Retro Heart Sneaker Grau',
+        cardPrice: '25 624,38 ₽',
+        showWrapper: ` 
+        <img src="/img/view_product.svg" alt=""> 
+        <img src="/img/add_about_busket.svg" alt=""> 
+        `,
+        shoesSize: 40,
+        cardsGender: 'Мужик'
+    },
+    {
+        id: 7,
+        imgUrl:
+            'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRAbuHYR6pXdjPeoa8aUDzahlT1SO9deTyWGWyH_p32JmsVJPbIkp8SRMDERFeCnSt2jTF84kncxSiaRIlUaQ0MECBT3mpxHesShTwShCY48mc6_X3UEgA6&usqp=CAc',
+        cardDesc: 'Кроссовки мужские Adidas Originals Campus 00s',
+        cardPrice: '11 841,84 ₽',
+        showWrapper: ` 
+        <img src="/img/view_product.svg" alt=""> 
+        <img src="/img/add_about_busket.svg" alt=""> 
+        `,
+        shoesSize: 41,
+        cardsGender: 'Мужик'
+    },
+    {
+        id: 8,
+        imgUrl:
+            'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcTAQN2kP2rhP_zJm7sFpZjof0xd1w7fDcsTwZLk4Kzm5u_jj_CTZcd40HDQzPVwanQ7pE8sTbm7tVdmF5o-sLW2xa4-phmELEcaDMZrev-xgbYIz9lfhqunyNzYcPvE5HZnTnIPtW8&usqp=CAc',
+        cardDesc: 'Кроссовки мужские Karl Kani 89 Lxry Prm',
+        cardPrice: '18 100,84 ₽',
+        showWrapper: ` 
+        <img src="/img/view_product.svg" alt=""> 
+        <img src="/img/add_about_busket.svg" alt=""> 
+        `,
+        shoesSize: 39,
+        cardsGender: 'Мужик'
+    },
+    {
+        id: 9,
+        imgUrl:
+            'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcT_dRvrfAgCiOXh-v2nO6b9xv46m49DHdHUmEI9ZsyjTbmKNBCjl7tfaKRq1XcAFpFzg2oIobldBW8MIyGXOu7k8k1BlTTJFUpoFndMrDSOe7xh6DfKPlBe_Yw&usqp=CAc',
+        cardDesc: 'Кроссовки мужские Sportlich Sneakers mit für Farbblock/43',
+        cardPrice: '3 648,88 ₽',
+        showWrapper: ` 
+        <img src="/img/view_product.svg" alt=""> 
+        <img src="/img/add_about_busket.svg" alt=""> 
+        `,
+        shoesSize: 38,
+        cardsGender: 'Мужик'
+    },
+    {
+        id: 10,
+        imgUrl:
+            'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcSzzmEdLLrXMpg7bKux7CbVxHwD5vtgHHwMSyaCqd9eHFUicvFVMkC5Veyfss-Xk0l6yW5PbSBVerdoqqCClAlY0Tb_mL9YzSsSljvOkKrLFiOdtty1FGnSK3luyKg0PhwjsjsSVOBz9w&usqp=CAc',
+        cardDesc: 'Кроссовки женские New Balance 550 in White',
+        cardPrice: '29 583,50 ₽',
+        showWrapper: ` 
+        <img src="/img/view_product.svg" alt=""> 
+        <img src="/img/add_about_busket.svg" alt=""> 
+        `,
+        shoesSize: 40,
+        cardsGender: 'Баба'
+    },
+    {
+        id: 11,
+        imgUrl:
+            'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQvkAaCIAK5a69H3oavpyO1pikwCzWiK1hCoOVDqISqMrWrBD6FRDOH_udoxqzjy6H9NtrPyZhaAxNBoZ5a5wwhkG6u1wmBPnJBNUp2v5vRIEyfastreD-qdg&usqp=CAc',
+        cardDesc: 'Кроссовки женские Steve Madden Possession Trainers',
+        cardPrice: '10 293,90 ₽',
+        showWrapper: ` 
+        <img src="/img/view_product.svg" alt=""> 
+        <img src="/img/add_about_busket.svg" alt=""> 
+        `,
+        shoesSize: 42,
+        cardsGender: 'Баба'
+    },
+    {
+        id: 12,
+        imgUrl:
+            'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQmn8NxKrq6w33CrFRI0Z4QfWtoM7DANvCZ_DT64pS9IwD6ycFiy4hGgFZxeDQ2wHp8KRFptIot5GEOSjpMzX0u60yYPubV0PfSFt25JxrZBY9agS53iQsKZ7loxFIDECPojncIou9CxA&usqp=CAc',
+        cardDesc: 'Кроссовки женские Adidas Forum Bold',
+        cardPrice: '6 902,37 р',
+        showWrapper: ` 
+        <img src="/img/view_product.svg" alt=""> 
+        <img src="/img/add_about_busket.svg" alt=""> 
+        `,
+        shoesSize: 43,
+        cardsGender: 'Баба'
+    },
+    {
+        id: 13,
+        imgUrl:
+            'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTMRDmOMG6dnRzW_NBT2wyzi1t5rF-w3pP6itStOYPDN-AVLr2qkNThXPM0wV9n_1_5Y_7ThTXFlNuscucfAIkbbLum_F_0ttFNwoDepUsktYb8w5T3wNeis6jTcep2ft-sdckFGJxQaA&usqp=CAc',
+        cardDesc: 'Кроссовки женские Men\'s Air 4 Retro Classic',
+        cardPrice: '12 320,82 ₽',
+        showWrapper: ` 
+        <img src="/img/view_product.svg" alt=""> 
+        <img src="/img/add_about_busket.svg" alt=""> 
+        `,
+        shoesSize: 41,
+        cardsGender: 'Баба'
+    },
+    {
+        id: 14,
+        imgUrl:
+            'https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSQlOUMfnJtr6ILZlPZsfb5nmctuHgQLV9QNQC7jRwUSUr0ay75KfGiQmmctSONaRojeqqK82JwVsezujtHX8yt5op2OrdxJ4nNIX2aES_1LoKBXlQf07aEBHFr8Vm6R0W235vV1Lk7qQ&usqp=CAc',
+        cardDesc: 'Кроссовки мужские Jordan Max Aura Men\'s',
+        cardPrice: '7 357,50 ₽',
+        showWrapper: ` 
+        <img src="/img/view_product.svg" alt=""> 
+        <img src="/img/add_about_busket.svg" alt=""> 
+        `,
+        shoesSize: 35,
+        cardsGender: 'Мужик'
+    },
+    {
+        id: 15,
+        imgUrl:
+            'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRQxgrrkMR3qBM8-H7fFCpdyWpTolF6BYU-NsHqCMQbPCtysE9nG6u9o_sxJkLXlUeAoa2HnCvCFKoQqAQ0Lft54WcNVSSq292UofpVbo_licAutzfiBqWb0c3OLRk0UyIykIk4IxX3JBY&usqp=CAc',
+        cardDesc: 'Кроссовки мужские Men\'s Air 4 Retro Calssic',
+        cardPrice: '23 583,20 ₽',
+        showWrapper: ` 
+        <img src="/img/view_product.svg" alt=""> 
+        <img src="/img/add_about_busket.svg" alt=""> 
+        `,
+        shoesSize: 43,
+        cardsGender: 'Мужик'
+    },
+    {
+        id: 16,
+        imgUrl:
+            'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcTggaAy56Cvr2p6OukYPC8Mnngyon6bu00X5Ubn8Wi9Ur_LR6x86G3MRO69CCUesrtvucFDLFJ-HGZYxFO5TuoZ7lNNCay14_xA_dbghkFVQ-d9pfIyZrTPAdhSV2EoDgNADJPDJmOjlQ&usqp=CAc',
+        cardDesc: 'Кроссовки мужские Adidas Originals Campus 00s',
+        cardPrice: '18 899,84 ₽',
+        showWrapper: ` 
+        <img src="/img/view_product.svg" alt=""> 
+        <img src="/img/add_about_busket.svg" alt=""> 
+        `,
+        shoesSize: 37,
+        cardsGender: 'Мужик'
+    },
+    {
+        id: 17,
+        imgUrl:
+            'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQGXWgSX-1_UvbKJrhI_lUJ6KCKViL5NBQ5HWpaXZmWmf7vxcOhaOn34J-nh6hsS7ONAVJpCWX9JGElvYFIKSpqWTKqrALc_mNR_Q6E1rjcTF5C0C25Dwq9SJ-YkUpstQVuhlGAKRdWNg&usqp=CAc',
+        cardDesc: 'Кроссовки мужские Nike Dunk Low Retro',
+        cardPrice: '14 299,84 ₽',
+        showWrapper: ` 
+        <img src="/img/view_product.svg" alt=""> 
+        <img src="/img/add_about_busket.svg" alt=""> 
+        `,
+        shoesSize: 42,
+        cardsGender: 'Мужик'
+    },
+    {
+        id: 18,
+        imgUrl:
+            'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTsC1UFAIlK96J01YCaTMtAi5QMHSrlKZVwK1USOft5b07FhMl-E1xV0g1e6gGvop4flG04asqjwSXzZua8O0yHcBRf3BygBGopYQ20qFNSLUaLj9PSCL_q84jfYAlAmPX6zcb95l3dmQ&usqp=CAc',
+        cardDesc: 'Кроссовки мужские New Balance Men\'s',
+        cardPrice: '29 999,99 ₽',
+        showWrapper: ` 
+        <img src="/img/view_product.svg" alt=""> 
+        <img src="/img/add_about_busket.svg" alt=""> 
+        `,
+        shoesSize: 36,
+        cardsGender: 'Мужик'
+    }
+]
+
+
+
+// Рендерит карточки
+let products = document.querySelector('.products');
+function renderCards(productData) {
+    products.innerHTML = '';
+    productData.forEach(item => {
+        let cardElement = `
+            <div class="cards">
+                <img src="${item.imgUrl}" alt="" class="cards__frame">
+                <div class="wrapper" style="display: none;">${item.showWrapper}</div>
+                <div class="cards__desc"><p>${item.cardDesc}</p></div>
+                <div class="cards__price"><p>${item.cardPrice}</p></div>
+                <img src="/img/free-icon-plus-149145.png" alt="" class="add_plus">
+            </div>
+        `;
+        products.innerHTML += cardElement;
+    });
+
+    let moreButton = `
+        <button class="show_more">Показать еще</button>
+    `;
+    products.innerHTML += moreButton;
+
+
+
+
+    addCardEventListeners(); // Добавление обработчиков событий к карточкам
+}
+
+
+let cards = document.querySelectorAll('.cards');
+// Эта функция задает элементу wrapper свойство block, если карточка активная, или none, если не активная.
+function addCardEventListeners() {
+    const cards = document.querySelectorAll('.cards');
+    let timeout;
+    cards.forEach(card => {
+        card.addEventListener('click', function (event) {
+            if (event.target.className !== 'add_plus') {
+                const activeCard = card;
+                const wrapper = activeCard.querySelector('.wrapper');
+
+                wrapper.style.display = wrapper.style.display === 'block' ? 'none' : 'block';
+
+                cards.forEach(elem => {
+                    const cardWrapper = elem.querySelector('.wrapper');
+                    if (elem !== activeCard) {
+                        cardWrapper.style.display = 'none';
+                    }
+                });
+
+                if (wrapper.style.display === 'block') {
+                    clearTimeout(timeout);
+                    timeout = setTimeout(() => {
+                        wrapper.style.display = 'none';
+                    }, 10000);
+                }
+            }
+        });
+    });
+}
+
+// Функция убирает у объекта cardsPrice все пробелы, заменяет букву р на пустоту и , на .
+function convertPrice(price) {
+    return parseFloat(price.replace(/\s/g, '').replace('₽', '').replace(',', '.'));
+}
+
+// Сортируем цены в порядке возрастания.
+function cardSort(products) {
+    return products.sort((a, b) => convertPrice(a.cardPrice) - convertPrice(b.cardPrice));
+}
+
+
+
+let filtersSizeSquare = document.querySelectorAll('.filters_size_columns .square_size');
+
+let selectedSize = null;
+
+filtersSizeSquare.forEach(item => {
+    item.addEventListener('click', function () {
+        filtersSizeSquare.forEach(elem => {
+            elem.classList.remove('active');
+        });
+        item.classList.add('active');
+        selectedSize = parseInt(item.textContent)
+    });
+});
+
+// Фильтруем productData в диапазоне minPrice и maxPrice.
+function filterProducts(minPrice, maxPrice, gender, selectedSize) {
+    return productData.filter(item => {
+        let price = convertPrice(item.cardPrice);
+        let genderMath = gender.length === 0 || gender.includes(item.cardsGender);
+        let sizeMath = selectedSize === null || selectedSize === item.shoesSize
+        return price >= minPrice && price <= maxPrice && genderMath && sizeMath;
+    });
+}
+
+let fromNumber = document.querySelector('.from-number');
+let beforeNumber = document.querySelector('.before-number');
+let fromRange = document.querySelector('#from-range');
+let beforeRange = document.querySelector('#before-range');
+
+fromRange.addEventListener('input', function () {
+    fromNumber.textContent = this.value;
+});
+beforeRange.addEventListener('input', function () {
+    beforeNumber.textContent = this.value;
+});
+
+let applyBth = document.querySelector('.apply');
+let manCheckbox = document.querySelector('#man');
+let womanCheckbox = document.querySelector('#woman');
+// При нажатии на кнопку apply срабатывают фильтры.
+
+applyBth.addEventListener('click', function () {
+    let minPrice = parseInt(fromRange.value);
+    let maxPrice = parseInt(beforeRange.value);
+
+    let genders = [];
+
+    if (manCheckbox.checked) genders.push('Мужик');
+    if (womanCheckbox.checked) genders.push('Баба');
+
+    if (minPrice === maxPrice && genders.length === 0) {
+        renderCards(productData);
+        return
+    }
+
+
+
+
+    let filteredProductsData = filterProducts(minPrice, maxPrice, genders, selectedSize);
+    renderCards(filteredProductsData);
+});
+
+// Сортировка и первоначальная отрисовка карточек
+let sortedProductData = cardSort(productData);
+renderCards(sortedProductData);
+
+
+let resetBtn = document.querySelector('.reset');
+
+resetBtn.addEventListener('click', function () {
+    fromRange.value = fromRange.min;
+    beforeRange.value = beforeRange.max;
+    fromNumber.textContent = fromRange.min;
+    beforeNumber.textContent = beforeRange.max;
+
+    selectedSize = null;
+    filtersSizeSquare.forEach(elem => {
+        elem.classList.remove('active');
+    });
+
+    genders = [];
+
+    renderCards(productData);
+});
+
+
+
+
+function updateTotalPrice() {
+    let total = 0;
+    let priceShoesBusket = document.querySelectorAll('.all-goods .price-shoes-busket');
+    let priceTotalBusket = document.querySelector('.price-total-busket')
+    priceShoesBusket.forEach(elem => {
+        let price = parseFloat(elem.textContent.replace(',', '.').replace(/\s/g, '').replace(' ₽', ''));
+        total += price;
+    });
+    priceTotalBusket.textContent = total.toFixed(1) + '₽'
+
+}
+
+function updateBusketIndicator() {
+    let menuOne = document.querySelector('.menu_one');
+    let number = menuOne.querySelector('.number');
+    let smallBusket = document.querySelectorAll('.all-goods .goods');
+
+    number.textContent = smallBusket.length;
+
+    if (smallBusket.length > 0) {
+        menuOne.classList.remove('hidden');
+    } else {
+        menuOne.classList.add('hidden');
+    }
+}
+
+function checkBusketEmpty() {
+    let busket = document.querySelector('.all-goods');
+    let goods = busket.querySelectorAll('.goods');
+
+
+
+    if (goods.length === 0) {
+        busket.innerHTML = `<div class="empty-message">Корзина пуста</div>`
+
+    }
+
+}
+
+function addToBusket(product) {
+    let busket = document.querySelector('.all-goods');
+
+
+    if (busket.querySelector('.empty-message')) {
+        busket.innerHTML = '';
+    }
+    let createGoods = document.createElement('div');
+    createGoods.className = 'goods';
+    createGoods.innerHTML = `<img src="${product.imgUrl}" alt="" class="icon-shoes-busket">
+<div class="sibling__goods">
+    <div class="desc-shoes-busket">${product.desc}</div>
+    <div class="price-shoes-busket">${product.price.toLocaleString()} ₽</div>
+</div>
+<img src="/img/sibling-busket.svg" alt="" class="sibling_busket">
+`;
+    busket.appendChild(createGoods);
+
+    createGoods.querySelector('.sibling_busket').addEventListener('click', function () {
+        createGoods.remove()
+        updateTotalPrice()
+        updateBusketIndicator()
+        checkBusketEmpty()
+    })
+    checkBusketEmpty();
+    updateTotalPrice();
+    updateBusketIndicator();
+}
+
+document.querySelectorAll('.add_plus').forEach(bth => {
+    bth.addEventListener('click', function () {
+        let card = bth.closest('.cards');
+        let product = {
+            imgUrl: card.querySelector('.cards__frame').src,
+            desc: card.querySelector('.cards__desc').innerHTML,
+            price: parseFloat(card.querySelector('.cards__price').textContent.replace(',', '.').replace(/\s/g, '').replace(' ₽', ''))
+        }
+        addToBusket(product)
+    })
+})
+
+document.querySelector('.complex_img').addEventListener('click', function () {
+    let busket = document.querySelector('.small-basket')
+    if (busket.style.display === 'none' || busket.style.display === '') {
+        busket.style.display = 'block'
+    } else {
+        busket.style.display = 'none'
+    }
+})
+checkBusketEmpty();
+updateTotalPrice();
+updateBusketIndicator();
